e24c3602cb9c3d1a3d1f57966d198759e2aa0ed5
RelinDmitrii <relin-dmitrii@yandex.ru>
2020-12-27 13:01:46 +0300

    f

---
+++ Server/src/main/java/Server/AuthService.java
@@ -0,0 +1,12 @@
+package Server;
+
+public interface AuthService {
+    /**
+     *
+     * @return - nickname если пользователь есть, null если пользователья нет
+     */
+    String getNicknameByLoginAndPassword(String login, String password);
+
+    boolean registration(String login, String password, String nickname);
+
+}
